277d23cf74ed4bbeb2e10c5dfd4c1a8119b4f3c1
Leham Greeves <Leham.Greeves@gmail.com>
2022-06-28 23:54:34 +0200

    first commit

--- base_app.py
+++ base_app.py
@@ -22,14 +22,38 @@
 
 """
 # Streamlit dependencies
-import streamlit as st
-import joblib,os
+#streamlit dependencies
 
-# Data dependencies
+import streamlit as st
+import joblib, os
+
+## data dependencies
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#from textblob import TextBlob
+import re
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+#from nlppreprocess import NLP # pip install nlppreprocess
+#import en_core_web_sm
+from nltk import pos_tag
+
+# Imports for data visualisation
+import seaborn as sns
+from scipy import stats
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_roc_curve
+from wordcloud import WordCloud
+import warnings
+warnings.filterwarnings('ignore')
+
 
 # Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
+news_vectorizer = open("resources/TfidfVectorizer.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
@@ -38,45 +62,238 @@ raw = pd.read_csv("resources/train.csv")
 # The main function where we will build the actual app
 def main():
 	"""Tweet Classifier App with Streamlit """
-
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
 	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+	st.header("Climate change tweet classification")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+	options = ["Information", "Tweet Classifier", "Analysis and Visuals", 'Contact App Developers']
+	selection = st.sidebar.selectbox("Navigater", options)
 
 	# Building out the "Information" page
 	if selection == "Information":
-		st.info("General Information")
+		st.subheader("Classification")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
+		st.markdown("We, as human beings, make multiple decisions throughout the day. For example, when to wake up, what to wear, who to call, which route to take when traveling, how to sit, the list goes on and on. While several of these are repetitive and we do not usually take notice (and allow it to be done subconsciously), there are many others that are new and require conscious thought.  And we learn along the way. Businesses, similarly, apply their past learning to decision-making related to operations and new initiatives e.g. relating to customer classification, products, etc. However, it gets a little more complex here as there are multiple stakeholders involved. Additionally, the decisions need to be accurate owing to their wider impact. With the evolution in digital technology, humans have developed multiple assets; machines being one of them. We have learned (and continue) to use machines for analyzing data using statistics to generate useful insights that serve as an aid in making decisions and forecasts.")
+		st.subheader("Logistic Regression")
+		st.markdown('Logistic Regression utilizes the power of regression to do classification and has been doing so exceedingly well for several decades now, to remain amongst the most popular models. One of the main reasons for the model’s success is its power of explainability i.e. calling out the contribution of individual predictors, quantitatively. Unlike regression which uses Least Squares, the model uses Maximum Likelihood to fit a sigmoid curve on the target variable distribution. Given the model’s susceptibility to multi-collinearity, applying it step-wise turns out to be a better approach in finalizing the chosen predictors of the model. The algorithm is a popular choice in many natural language processing tasks e.g. toxic speech detection, topic classification, etc...')
+		st.subheader('Random Forest')
+		st.markdown('A Random Forest is a reliable ensemble of multiple Decision Trees (or CARTs); though more popular for classification, than regression applications. Here, the individual trees are built via bagging (i.e. aggregation of bootstraps which are nothing but multiple train datasets created via sampling of records with replacement) and split using fewer features. The resulting diverse forest of uncorrelated trees exhibits reduced variance; therefore, is more robust towards change in data and carries its prediction accuracy to new data. However, the algorithm does not work well for datasets having a lot of outliers, something which needs addressing prior to the model building.')
+		st.subheader('Naïve Bayes')
+		st.markdown('Though we may not realize it, this is the algorithm that’s most commonly used to sift through spam emails! It applies what is known as a posterior probability using Bayes Theorem to do the categorization of the unstructured data. And in doing so, it makes a naïve assumption that the predictors are independent, which may not be true. The model works well with a small training dataset, provided all the classes of the categorical predictor are present.')
+		st.subheader('KNN')
+		st.markdown('K-Nearest Neighbor (KNN) algorithm predicts based on the specified number (k) of the nearest neighboring data points. Here, the pre-processing of the data is significant as it impacts the distance measurements directly. Unlike others, the model does not have a mathematical formula, nor any descriptive ability. Here, the parameter ‘k’ needs to be chosen wisely; as a value lower than optimal leads to bias, whereas a higher value impacts prediction accuracy. It is a simple, fairly accurate model preferable mostly for smaller datasets, owing to the huge computations involved in the continuous predictors. At a simple level, KNN may be used in a bivariate predictor setting e.g. height and weight, to determine the gender, given a sample.')
+		st.subheader('Passive Aggressive Classifier')
+		st.markdown('Passive Aggressive Classifier belongs to the category of online learning algorithms in machine learning. It works by responding as passive for correct classifications and responding as aggressive for any miscalculation.')
+		st.subheader('Bernoulli Naive Bayes')
+		st.markdown('Bernoulli Naive Bayes is one of the variants of the Naive Bayes algorithm in machine learning. It is very useful to be used when the dataset is in a binary distribution where the output label is present or absent. The main advantage of this algorithm is that it only accepts features in the form of binary values. ')
+		st.subheader('Stacking Classifier')
+		st.markdown('Stacking is an ensemble learning technique to combine multiple classification models via a meta-classifier. The individual classification models are trained based on the complete training set; then, the meta-classifier is fitted based on the outputs -- meta-features -- of the individual classification models in the ensemble. ')
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
 	# Building out the predication page
-	if selection == "Prediction":
+	if selection == "Tweet Classifier":
 		st.info("Prediction with ML Models")
+		
+		st.markdown('Insert a climate change related tweet and see the predicted sentiment!')
+		st.markdown('The focus of this section is to:')
+		st.markdown('1. Insert a tweet in the textbox.')
+		st.markdown('2. Choose one of the various models for prediction')
+		st.markdown('3. Learn if the tweet supports or opposes man-made climate change')
 		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
-
-		if st.button("Classify"):
+		tweet_text = st.text_area("Enter Text Here:","Type Here")
+		
+		if st.button("Logistic Regression Classifer"):
 			# Transforming user input with vectorizer
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			predictor = joblib.load(open(os.path.join("resources/logreg.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				result = 'Neutral: the tweet neither supports nor refutes the belief of man-made climate change'
+			elif prediction == 1:
+				result = 'Pro: the tweet supports the belief of man-made climate change'
+			elif prediction == 2:
+				result = 'News: the tweet links to factual news about climate change'	
+			else:
+				result = 'Anti: the tweet does not believe in man-made climate change'		
+			st.success(result)
+			
 
+		if st.button("Random Forest Classifier"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/rfc.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				result = 'Neutral: the tweet neither supports nor refutes the belief of man-made climate change'
+			elif prediction == 1:
+				result = 'Pro: the tweet supports the belief of man-made climate change'
+			elif prediction == 2:
+				result = 'News: the tweet links to factual news about climate change'	
+			else:
+				result = 'Anti: the tweet does not believe in man-made climate change'		
+			st.success(result)
+
+		if st.button("Linear Support Vector Classifier"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/lsvc_model.pkl"),"rb"))
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				result = 'Neutral: the tweet neither supports nor refutes the belief of man-made climate change'
+			elif prediction == 1:
+				result = 'Pro: the tweet supports the belief of man-made climate change'
+			elif prediction == 2:
+				result = 'News: the tweet links to factual news about climate change'	
+			else:
+				result = 'Anti: the tweet does not believe in man-made climate change'		
+			st.success(result)
+
+		if st.button("Naive Bayes Classifier"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/naive_bayes.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+			# When model has successfully run, will print prediction
+			# You can use a dictionary or similar structure to make this output
+			# more human interpretable.
+			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				result = 'Neutral: the tweet neither supports nor refutes the belief of man-made climate change'
+			elif prediction == 1:
+				result = 'Pro: the tweet supports the belief of man-made climate change'
+			elif prediction == 2:
+				result = 'News: the tweet links to factual news about climate change'	
+			else:
+				result = 'Anti: the tweet does not believe in man-made climate change'		
+			st.success(result)
+		
+		if st.button("Bernoulli Naive Bayes Classifier"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/BNBmodel.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				result = 'Neutral: the tweet neither supports nor refutes the belief of man-made climate change'
+			elif prediction == 1:
+				result = 'Pro: the tweet supports the belief of man-made climate change'
+			elif prediction == 2:
+				result = 'News: the tweet links to factual news about climate change'	
+			else:
+				result = 'Anti: the tweet does not believe in man-made climate change'		
+			st.success(result)
+
+		if st.button("Passive-Aggressive Classifier"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/passive_tf.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				result = 'Neutral: the tweet neither supports nor refutes the belief of man-made climate change'
+			elif prediction == 1:
+				result = 'Pro: the tweet supports the belief of man-made climate change'
+			elif prediction == 2:
+				result = 'News: the tweet links to factual news about climate change'	
+			else:
+				result = 'Anti: the tweet does not believe in man-made climate change'		
+			st.success(result)
+
+		if st.button("Stacking Classifier"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/StackingClassifier.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				result = 'Neutral: the tweet neither supports nor refutes the belief of man-made climate change'
+			elif prediction == 1:
+				result = 'Pro: the tweet supports the belief of man-made climate change'
+			elif prediction == 2:
+				result = 'News: the tweet links to factual news about climate change'	
+			else:
+				result = 'Anti: the tweet does not believe in man-made climate change'		
+			st.success(result)
+
+	# Building out the predication page
+	if selection == "Analysis and Visuals":
+		st.info("Visual Analysis")
+		st.markdown('In this section we take a closer look at our data using visual analysis ')
+
+		st.markdown('(1) 52.3% of the tweets support the belief of man-made climate change ')
+		st.markdown('(2) 21.1% of the tweets link to factual news about climate change')
+		st.markdown('(0) 17.6% of the tweets neither support nor refute the belief of man-made climate change')
+		st.markdown('(-1) 9.1% of the tweets do not believe in man-made climate change.')
+		raw['sentiment'] = [['Negative', 'Neutral', 'Positive', 'News'][x+1] for x in raw['sentiment']]# checking the distribution
+		st.subheader('Pie chart visual of sentiment distribution')
+		values = raw['sentiment'].value_counts()/raw.shape[0]
+		labels = (raw['sentiment'].value_counts()/raw.shape[0]).index
+		colors = ['olive', 'greenyellow', 'palegreen', 'g']
+		plt.pie(x=values, labels=labels, autopct='%1.1f%%', startangle=90, explode= (0.1, 0.1, 0.1, 0.1), colors=colors)
+		st.pyplot()
+
+		st.subheader('Wordcloud chart visual of sentiment distribution')
+		st.markdown('The word cloud is a visualisation that represents the most frequently used words in the given data')
+		st.markdown('')
+		all_words = " ".join([sentence for sentence in raw["message"]])
+		wordcloud = WordCloud(width=800, height=500, random_state=42, max_font_size=100).generate(all_words)
+		#plot graph
+		plt.figure(figsize=(15,8))
+		plt.imshow(wordcloud, interpolation= 'bilinear')
+		plt.axis('off')
+		st.pyplot()
+		# Visualize frequent words
+		st.subheader('Main words used:')
+		st.markdown('Climate change')
+		st.markdown('Global warming')
+		st.markdown('Believe')
+		st.markdown('Scientist')
+		st.markdown('Hoax')
+		st.markdown('Change')
+		st.markdown('President / Trump')
+		st.markdown('Earth')
+		st.markdown('Action')
+		st.markdown('Combat')
+		st.markdown(' Fight')
+		st.markdown('American')
+
+	if selection == 'Contact App Developers':
+		st.info('Contact details in case you any query or would like to know more of our designs:')
+		st.write('Keara: kbarnard625@gmail.com')
+		st.write('Ronewa: Mutobvuronewa@gmail.com')
+		st.write('Leham: leham.greeves@gmail.com')
+		st.write('Cecilia: Cecilianunguiane@gmail.com')
+		st.write('Ayanda: ayanda7397@gmail.com')
+		st.write('Siya: Siya.xola94@gmail.com')
+## Looking at the main words used throughout these tweets, we can see that there are many conflicting beliefs.
+## Some belief that climate change is:
+## * man-made
+## * political agenda being pushed
+## * a hoax
+## * occuring only in USA
+## * other
+
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
