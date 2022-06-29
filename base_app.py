536c111c97498c3668c4a3a68fea94bdf88f64f4
Leham-Greeves <100915330+Leham-Greeves@users.noreply.github.com>
2022-06-29 18:25:57 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -275,7 +275,7 @@ def main():
 
 	if selection == 'Contact App Developers':
 		
-		img = Image.open("https://github.com/KearaB/CBB2-Classification-APP-predict/blob/master/Desktop/WhatsApp%20Image%202022-06-29%20at%206.05.11%20PM.jpeg")
+		img = Image.open("WhatsApp Image 2022-06-29 at 6.05.11 PM.jpeg")
 		# display image using streamlit
 		# width is used to set the width of an image
 		st.image(img, width=200)
